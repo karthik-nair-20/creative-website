10a4aaaa27772f75c48f16d2c39701dcfa5d5a5e
Karthik Nair <herbolax77@gmail.com>
2023-07-26 11:29:55 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.toggle')
+const nav = document.querySelector('.nav')
+
+toggle.addEventListener('click', () =>{
+    toggle.classList.toggle('active')
+    nav.classList.toggle('active')
+
+})
